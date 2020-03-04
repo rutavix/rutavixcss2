43181bc695abe53458e48e502f033f6744070e98
rutavix <rutavix@gmail.com>
2020-03-04 17:37:02 +0000

    changes

---
+++ js/js.js
@@ -0,0 +1,20 @@
+//alert('Hello!');
+
+let greetings = 'World!';
+//alert(greetings);
+
+const message = 'Hello ' + greetings;
+alert(message);
+
+const userAge = parseFloat(prompt('Enter your age'));
+alert('Yor age is: ' + userAge);
+ 
+const nextAge = userAge +1;
+alert(nextAge);
+
+if (userAge >= 18) {
+    alert('You are adult!');
+} else {
+    alert('You are not adult!');
+}
+
